0c5ef1dacc1b3af617c93cac469b5f207d4f8af6
dohieu070404 <dohieu07042004@gmail.com>
2024-12-02 21:48:49 +0700

    Delete lap1 directory

--- lap1/FirtsDialog.java
+++
@@ -1,9 +0,0 @@
-package lap1;
-import javax.swing.JOptionPane;
-public class FirtsDialog {
-	public static void main(String[] args){
-		JOptionPane.showMessageDialog(null, "Xin chao ban!" );
-
-		System.exit(0);
-	}
-}
--- lap1/helloWorld.java
+++
@@ -1,9 +0,0 @@
-
-package lap1;
-
-public class helloWorld { 
-	public static void main(String args[]){
-		System.out.println( "Xin chao \n cac ban!");
-		System.out.println( "Hello \t World!");
-	}
-}
